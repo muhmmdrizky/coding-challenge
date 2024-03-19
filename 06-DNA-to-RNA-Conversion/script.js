8bc6e203e9159071683eefe6165415702987af7e
Muhammad Rizky <muhmmdrizkymr@gmail.com>
2024-03-19 22:10:53 +0700

    coding challenge

--- 06-DNA-to-RNA-Conversion/script.js
+++ 06-DNA-to-RNA-Conversion/script.js
@@ -15,3 +15,9 @@ For example:
 "GCAT"  =>  "GCAU"
 The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
 */
+
+function DNAtoRNA(dna) {
+  // create a function which returns an RNA sequence from the given DNA sequence
+  return dna.replaceAll("T", "U");
+}
+console.log(DNAtoRNA("TTTT"));
